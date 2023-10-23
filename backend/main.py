70c52db487cbc565afffe5612a4162209f621045
Matias-Bustamante <matybustamante151@gmail.com>
2023-10-23 12:35:48 -0300

    Se agregó dashboard de looker studio en streamlit

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,12 @@ from streamlit_kpi import streamlit_kpi
 from st_aggrid import AgGrid,GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 from streamlit_elements import html, mui, elements
+from streamlit_extras.metric_cards import style_metric_cards 
+import streamlit.components.v1 as components
 import pandas as pd 
 import time 
 
+        
 
 extract=Extract(); 
 data=extract.extract_data(); 
@@ -23,9 +26,6 @@ data=transform.cambiarFormatoFechas(data)
 dataEstado=transform.ObtenerEstado(data)
 TareasTerminadas=consulta.CantidadTareasCompletadas(data); 
 
-
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 menu=option_menu( 
         menu_title=None, 
@@ -43,11 +43,11 @@ menu=option_menu(
     )
 col1,col2=st.columns(2)
 if menu=="Tareas": 
-    
    
     with col1: 
         
         st.metric("Tareas completadas", value=TareasTerminadas)
+        style_metric_cards(border_left_color="yellow", box_shadow=True, border_radius_px=10)
     with elements("new_element"): 
        mui.Typography("Listas de Tareas",variant="h3", mt={4}, Type="left", fontSize="3rem", color="blue")
     
@@ -72,10 +72,11 @@ if menu=="Tareas":
            )
     
     
+if menu=='Dashboard': 
     
-    
-    
-    
+    st.markdown("""
+    <iframe  src="https://lookerstudio.google.com/embed/reporting/033203f2-3241-4f80-9458-3de74dbc8364/page/TQgdD" frameborder="0" seamless="seamless"  style="position:absolute;top:0px;left:0px;bottom:0px;right:0px;width:100vw;height:100vh; border:none;margin:0;padding:0;overflow:hidden;z-index:999999;" allowfullscreen></iframe>
+    """, unsafe_allow_html=True)
     
 
 
